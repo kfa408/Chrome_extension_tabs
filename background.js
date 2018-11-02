f410e545905036ae0b56058969d3d948d4fd1c8b
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-11-02 14:56:12 -0700

    updated the extension to add/delete groups of tabs

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
-    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
@@ -11,10 +8,3 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
     });
 });
-
-// chrome.tabs.onActivated.addListener(function(){
-//     chrome.tabs.query({'highlighted': true}, function(tabs) {
-//         console.log(tabs);
-//         chrome.tabs.update(tabs[0].id, {url: newUrl});
-//     });
-// });
--- popup.js
+++ popup.js
@@ -1,26 +1,85 @@
 let changeColor = document.getElementById('changeColor');
 let showTabs = document.getElementById('tabs');
+let saveButton = document.getElementById('save');
+let currentTabs;
+
+let genA = (tab)=>{
+    let a = document.createElement('a');
+    let linkText = document.createTextNode(tab.title);
+    a.appendChild(linkText);
+    a.title = tab.title;
+    a.href = tab.url;
+    a.onclick = ()=>{
+        chrome.tabs.create({active: true, url: tab.url});
+    }
+    return a;
+}
+
+let genOpenTabs = (name, inputList)=>{
+    let btn = document.createElement('BUTTON');
+    let linkText = document.createTextNode(name);
+    btn.appendChild(linkText);
+    btn.onclick = ()=>{
+        inputList.forEach((input)=>{
+            chrome.tabs.create({active: true, url: input.url});
+        })
+    }
+    return btn;
+}
+
+let genDelete = (name)=>{
+    let btn = document.createElement('BUTTON');
+    let linkText = document.createTextNode('D');
+    btn.appendChild(linkText);
+    btn.onclick = ()=>{
+        let elem = document.getElementById(name);
+        elem.parentNode.removeChild(elem);
+        chrome.storage.sync.remove([name], ()=>{
+            console.log(`removed ${name}`);
+        });
+    };
+    return btn;
+}
+
+let genUl = (name, inputList) =>{
+    let container = document.createElement('div');
+    container.id = name;
+    let listName = genOpenTabs(name, inputList);
+    let deleteButton = genDelete(name);
+    let newList = document.createElement('ul');
+    inputList.forEach((input)=>{
+        let item = document.createElement('li');
+        item.appendChild(genA(input));
+        newList.appendChild(item);
+    });
+    container.appendChild(listName);
+    container.appendChild(deleteButton);
+    container.appendChild(newList);
+    document.body.appendChild(container);
+}
+
+chrome.storage.sync.get(function(results){
+    console.log(results);
+    Object.entries(results).forEach(([key, value]) =>{
+        // console.log(key, value);
+        genUl(key, value);
+    });
+});
+
 
 chrome.tabs.query({'highlighted': true}, function(tabs) {
-    console.log(showTabs);
     tabs.forEach((tab)=>{
-        // console.log(tab);
         let item = document.createElement('li');
         item.textContent = tab.title;
         showTabs.appendChild(item);
     })
-    // chrome.tabs.update(tabs[0].id, {url: newUrl});
+    currentTabs = tabs;
 });
-//
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-// changeColor.onclick = function(element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.executeScript(
-//         tabs[0].id,
-//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//   });
-// };
+
+saveButton.onclick = ()=>{
+    let curName = document.getElementById('curName').value;
+    genUl(curName, currentTabs);
+    chrome.storage.sync.set({[curName]: currentTabs}, function(){
+        console.log(currentTabs);
+    });
+}
